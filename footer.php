6e3015cf7b8fb72b5321edf0f2f08b81e3689c5f
Kristof Buts <github@kristofbuts.be>
2018-04-07 20:56:25 +0200

    scss + footer abstraction

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: butskristof
+ * Date: 07/04/2018
+ * Time: 20:43
+ */
+?>
+<hr>
+
+<footer class="container">
+		<span class="float-left icons">
+<!--			FB-->
+			<a href="https://facebook.com/sotromusic">
+                <i class="fab fa-facebook fa-lg"></i>
+			</a>
+<!--			INSTA-->
+            <a href="https://www.instagram.com/iclean1/">
+                <i class="fab fa-instagram fa-lg"></i>
+            </a>
+<!--			EMAIL-->
+<!--			TODO contact form of bot ignore -->
+			<a href="roemel@kristofbuts.be">
+                <i class="far fa-envelope fa-lg"></i>
+			</a>
+<!--			WHATSAPP -->
+			<a href="https://api.whatsapp.com/send?phone=32477430865">
+				<i class="fab fa-whatsapp fa-lg"></i></a>
+		</span>
+    <span class="float-right">
+            <p>&copy; 2018 iClean</p>
+		</span>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
