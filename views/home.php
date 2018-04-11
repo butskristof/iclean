6090c6173cdc8f23222e11cb81a95ad077eee0c0
Kristof Buts <github@kristofbuts.be>
2018-04-11 13:04:54 +0200

    Home page done

--- views/home.php
+++ views/home.php
@@ -37,9 +37,11 @@
 				<img src="/static/img/foto/trui.jpg" alt="Mitch Falchou, zaakvoerder" class="img-fluid">
 			</div><!-- /col-md-4 -->
 			<div class="col-md-8" id="jumbo_sec_text">
-				<h2>Koptekst</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec diam et augue tincidunt rhoncus sagittis a magna. Nam mollis bibendum lacus, vitae pharetra lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam et massa non orci elementum lacinia eu at ex. Proin enim nisl, feugiat id scelerisque sed, imperdiet nec massa. Vivamus imperdiet erat a tortor pulvinar tincidunt. Vestibulum porttitor posuere diam, quis ullamcorper metus malesuada in. Duis luctus orci quis nisi vehicula accumsan. </p>
-				<p><a class="btn btn-secondary" href="/diensten" role="button">Onze diensten &raquo;</a></p>
+				<div>
+					<h2>Koptekst</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec diam et augue tincidunt rhoncus sagittis a magna. Nam mollis bibendum lacus, vitae pharetra lectus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam et massa non orci elementum lacinia eu at ex. Proin enim nisl, feugiat id scelerisque sed, imperdiet nec massa. Vivamus imperdiet erat a tortor pulvinar tincidunt. Vestibulum porttitor posuere diam, quis ullamcorper metus malesuada in. Duis luctus orci quis nisi vehicula accumsan. </p>
+					<p><a class="btn btn-secondary" href="/diensten" role="button">Onze diensten &raquo;</a></p>
+				</div><!-- /id="jumbo_sec_text" -->
 			</div><!-- /col-md-8 -->
 		</div><!-- /row -->
 	</div><!-- /container -->
@@ -47,3 +49,21 @@
 
 
 <!-- SOCIALS -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 info_col">
+			<h2>Instagram</h2>
+
+<!--			todo upgrade to full version-->
+			<!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/6f2be986000c59288ad94e80eaee2b5b.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+
+		</div><!-- /col-md-6 -->
+		<div class="col-md-6 info_col">
+			<h2>Facebook</h2>
+
+<!--			todo famax embed fb -->
+
+		</div><!-- /col-md-6 -->
+	</div><!-- /row -->
+</div><!-- /container -->
